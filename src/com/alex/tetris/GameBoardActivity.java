ee96fb8e2c9738e99d9ee66c302e29f3c2f9d98f
Alexandre DUHEM <alexandreduhem@gmail.com>
2013-10-19 17:00:40 +0200

    création de la gameboardview - petite UI pour l'activity

--- src/com/alex/tetris/GameBoardActivity.java
+++ src/com/alex/tetris/GameBoardActivity.java
@@ -2,14 +2,20 @@ package com.alex.tetris;
 
 import android.app.Activity;
 import android.os.Bundle;
+import com.alex.tetris.widget.GameBoardView;
 
 public class GameBoardActivity extends Activity {
-    /**
-     * Called when the activity is first created.
-     */
+
+    private GameBoardView gameBoardView;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.game_board_activity);
+        init();
+    }
+
+    private void init() {
+        gameBoardView = (GameBoardView) findViewById(R.id.view_gameboard);
     }
 }
---
+++ src/com/alex/tetris/widget/GameBoardView.java
@@ -0,0 +1,47 @@
+package com.alex.tetris.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: alexandreduhem
+ * Date: 19/10/13
+ * Time: 16:40
+ * To change this template use File | Settings | File Templates.
+ */
+public class GameBoardView extends View {
+
+    private static final int DEFAULT_BOARD_HEIGHT = 18;
+    private static final int DEFAULT_BOARD_LENGTH = 10;
+
+
+    public GameBoardView(Context context) {
+        this(context, null);
+    }
+
+    public GameBoardView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public GameBoardView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    public void onButtonLeftClick(View view) {
+
+    }
+
+    public void onButtonRotateClick(View view) {
+
+    }
+
+    public void onButtonDownClick(View view) {
+
+    }
+
+    public void onButtonRightClick(View view) {
+
+    }
+}
